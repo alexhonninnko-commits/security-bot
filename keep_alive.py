e6030d7ecb1881752cc8f483b84ad33a6ae2d6c1
alexhonninnko-commits <alexhonninnko@gmail.com>
2026-09-25 17:42:58 +0200

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot i webova stranka bezi 24/7!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
